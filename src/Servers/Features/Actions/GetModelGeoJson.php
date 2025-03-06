549b835725d70a0e5d000b1c392f4c8abe1160e7
Boris Glumpler <boris@shabushabu.eu>
2025-03-06 13:25:17 +0545

    feat: added laravel 12 compatibility

--- src/Servers/Features/Actions/GetModelGeoJson.php
+++ src/Servers/Features/Actions/GetModelGeoJson.php
@@ -18,8 +18,8 @@ class GetModelGeoJson implements GetsModelGeoJson
     {
         $select = collect($model->geoJsonColumns())->map(
             fn (string | Expression $alias, string | int $column) => match (true) {
-                $alias instanceof Expression => $alias,
                 is_int($column) && is_string($alias) => new Alias($alias, Str::camel($alias)),
+                $alias instanceof Expression => $alias,
                 default => new Alias($column, $alias),
             }
         )->values()->all();
